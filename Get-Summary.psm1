878a05538e030d17607247f8be916a42669e7fee
Prateek Singh <Prateeksingh1590@gmail.com>
2018-03-16 15:27:13 +0530

    Update Get-Summary.psm1

--- Get-Summary.psm1
+++ Get-Summary.psm1
@@ -7,7 +7,7 @@ Text File with the content to summarize.
 .PARAMETER WordLimit
 Maximum number of words to be allowed in Summary
 .EXAMPLE
-PS Root\> Get-Summary -File D:\Document.txt
+PS Root\> Get-Summary -File .\Document.txt
 
 “Since my letter [of October 28], the FBI investigation team has been working round-the-clock to process and review a large volume of emails from a device obtained in connection with an unrelated criminal investigation,” Mr. Comey said. It was reported that there were 6,50,000 emails on that laptop.
 
@@ -37,142 +37,142 @@ Use -FromClipboard switch to summarize the content copied to clipboard
 .LINK
 Get-Content
 .LINK
-http://geekeefy.wordpress.com/
+http://RidiCurious.com/
 
 .NOTES
 
 Author  : Prateek Singh 
 Twitter : @SinghPrateik
-Blog    : http://Geekeefy.wordpress.com
+Blog    : http://RidiCurious.com/
 
 #>
 Function Get-Summary
 {
-[cmdletbinding()]
-[Alias('Summary')]
-[OutputType([String])]
-Param(
-        [Parameter(Position=0)] [String] $File,
-        [Parameter(Position=1)] [Int] $WordLimit = 100,
+    [cmdletbinding()]
+    [Alias('Summary')]
+    [OutputType([String])]
+    Param(
+        [Parameter(Position = 0)] [String] $File,
+        [Parameter(Position = 1)] [Int] $WordLimit = 100,
         [switch] $FromClipBoard
-)
+    )
 
-Begin
-{
-    If($File)
+    Begin
     {
-        $Content = Get-Content $File
-    }
-    elseif($FromClipBoard)
-    {
-        Add-Type -Assembly PresentationCore
-        $Content = [Windows.clipboard]::GetText()
-    }
-    else
-    {
-        Write-Host "Please provide a file path or copy content to Clipboard"
-    }
-}
-Process
-{
-    $TotalWords = 0
-    $Summary=@()
-    
-    #Extracting Best sentences with highest Ranks within the word limit
-    $BestSentences = Foreach($Item in (Get-SentenceRank $Content | Sort SentenceScore -Descending))
-    {
-        #Condition to limit Total word Count
-        $TotalWords += $Item.WordCount
-    
-        If($TotalWords -gt $WordLimit)
+        If ($File)
         {
-            break
+            $Content = Get-Content $File
+        }
+        elseif ($FromClipBoard)
+        {
+            Add-Type -Assembly PresentationCore
+            $Content = [Windows.clipboard]::GetText()
         }
         else
         {
-            $Item
+            Write-Host "Please provide a file path or copy content to Clipboard"
         }
     }
-    
-    If($BestSentences)
+    Process
     {
-
-        #Constructing a paragraph with sentences in Chronological order
-        Foreach($best in (($BestSentences |sort Linenumber).sentence))
+        $TotalWords = 0
+        $Summary = @()
+    
+        #Extracting Best sentences with highest Ranks within the word limit
+        $BestSentences = Foreach ($Item in (Get-SentenceRank $Content | Sort-Object SentenceScore -Descending))
         {
-            If(-not $Best.trim().endswith("."))
+            #Condition to limit Total word Count
+            $TotalWords += $Item.WordCount
+    
+            If ($TotalWords -gt $WordLimit)
             {
-                $Summary += -join ($Best, ".")
-            
+                break
             }
             else
             {
-                $Summary += -join ($Best, "")
+                $Item
+            }
+        }
+    
+        If ($BestSentences)
+        {
+
+            #Constructing a paragraph with sentences in Chronological order
+            Foreach ($best in (($BestSentences |Sort-Object Linenumber).sentence))
+            {
+                If (-not $Best.trim().endswith("."))
+                {
+                    $Summary += -join ($Best, ".")
+            
+                }
+                else
+                {
+                    $Summary += -join ($Best, "")
+                }
+        
             }
         
+            [String]$Summary
+
+            Write-Verbose "Content has been summarized from $($Content.split(" ").count) to $(([string]$Summary).split(" ").count) Words"
         }
-        
-        [String]$Summary
-
-        Write-Verbose "Content has been summarized from $($Content.split(" ").count) to $(([string]$Summary).split(" ").count) Words"
+        else
+        {
+            Write-Warning "Word Limit is too small to summarize the document."
+        }
     }
-    else
+    End
     {
-        Write-Warning "Word Limit is too small to summarize the document."
-    }
-}
-End
-{
 
-}
+    }
 
  
 }
 
 Function Get-Intersection($Sentence1, $Sentence2)
 {
-    $CommonWords = Compare-Object -ReferenceObject $Sentence1 -DifferenceObject $Sentence2 -IncludeEqual |?{$_.sideindicator -eq '=='} | select Inputobject -ExpandProperty Inputobject
-    $CommonWords.Count / ($Sentence1.Count + $Sentence2.Count) /2
+    $CommonWords = Compare-Object -ReferenceObject $Sentence1 -DifferenceObject $Sentence2 -IncludeEqual |Where-Object {$_.sideindicator -eq '=='} | Select-Object Inputobject -ExpandProperty Inputobject
+    $CommonWords.Count / ($Sentence1.Count + $Sentence2.Count) / 2
 }
 
 Function Get-SentenceRank($Content)
 {
-    $Sentences = $content -split [environment]::NewLine | ?{$_}
+    $Sentences = $content -split [environment]::NewLine | Where-Object {$_}
 
 
     $NoOfSentences = $Sentences.count
-    $values = New-Object 'object[,]' $NoOfSentences,$NoOfSentences
+    $values = New-Object 'object[,]' $NoOfSentences, $NoOfSentences
     $CommonContentWeight = New-Object double[] $NoOfSentences
     
     #Get important words that where length is greater than 3 to avoid - in, on, of, to, by etc
-    $FrequencyDistribution =  $Content.split(" ") |?{-not [String]::IsNullOrEmpty($_)} | %{[Regex]::Replace($_,'[^a-zA-Z0-9]','')} |group |sort count -Descending
-    $ImportantWords = $FrequencyDistribution |?{$_.name.length -gt 3} | select @{n='ImportanceWeight';e={$_.Count * 0.01}}, @{n='ImportantWord';e={$_.Name}} -First 10
+    $FrequencyDistribution = $Content.split(" ") |Where-Object {-not [String]::IsNullOrEmpty($_)} | ForEach-Object {[Regex]::Replace($_, '[^a-zA-Z0-9]', '')} |Group-Object |Sort-Object count -Descending
+    $ImportantWords = $FrequencyDistribution |Where-Object {$_.name.length -gt 3} | Select-Object @{n = 'ImportanceWeight'; e = {$_.Count * 0.01}}, @{n = 'ImportantWord'; e = {$_.Name}} -First 10
 
-    Foreach($i in (0..($NoOfSentences-1)))
+    Foreach ($i in (0..($NoOfSentences - 1)))
     {
         $ImportanceWeight = 0
 
         #Score each Sentence on basis of words common in every other sentence
         #More a sentence has common words from all other sentences, more it defines the complete document
                 
-        Foreach($j in (0..($NoOfSentences-1)))
+        Foreach ($j in (0..($NoOfSentences - 1)))
         {
-            $WordsInReferenceSentence = $Sentences[$i].Split(" ") | Foreach{[Regex]::Replace($_,'[^a-zA-Z0-9]','')}
-            $WordsInDifferenceSentence = $Sentences[$j].Split(" ") | Foreach{[Regex]::Replace($_,'[^a-zA-Z0-9]','')}
+            $WordsInReferenceSentence = $Sentences[$i].Split(" ") | ForEach-Object {[Regex]::Replace($_, '[^a-zA-Z0-9]', '')}
+            $WordsInDifferenceSentence = $Sentences[$j].Split(" ") | ForEach-Object {[Regex]::Replace($_, '[^a-zA-Z0-9]', '')}
         
             $CommonContentWeight[$i] = $CommonContentWeight[$i] + (Get-Intersection  $WordsInReferenceSentence $WordsInDifferenceSentence)
         }
 
-        Foreach($Item in $WordsInReferenceSentence |select -unique)
+        Foreach ($Item in $WordsInReferenceSentence |Select-Object -unique)
         {
             #Keep adding ImportanceWeight if an Important word found in the sentence
-            If($Item -in $ImportantWords.ImportantWord)
+            If ($Item -in $ImportantWords.ImportantWord)
             {
-                $ImportanceWeight += ($ImportantWords| ?{$_.ImportantWord -eq $Item}).ImportanceWeight
+                $ImportanceWeight += ($ImportantWords| Where-Object {$_.ImportantWord -eq $Item}).ImportanceWeight
             }
         }
     
-        ''| select  @{n='LineNumber';e={$i}},@{n='SentenceScore';e={"{0:N3}"-f ($CommonContentWeight[$i]+$ImportanceWeight)}} ,  @{n='CommonContentScore';e={"{0:N3}"-f $CommonContentWeight[$i]}}, @{n='ImportanceScore';e={$ImportanceWeight}}, @{n='WordCount';e={($Sentences[$i].Split(" ")).count}} , @{n='Sentence';e={$Sentences[$i]}}
+        ''| Select-Object  @{n = 'LineNumber'; e = {$i}}, @{n = 'SentenceScore'; e = {"{0:N3}" -f ($CommonContentWeight[$i] + $ImportanceWeight)}} , @{n = 'CommonContentScore'; e = {"{0:N3}" -f $CommonContentWeight[$i]}}, @{n = 'ImportanceScore'; e = {$ImportanceWeight}}, @{n = 'WordCount'; e = {($Sentences[$i].Split(" ")).count}} , @{n = 'Sentence'; e = {$Sentences[$i]}}
     }
 }
 
